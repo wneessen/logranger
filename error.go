8d75b19e27fc595dc77af9a8a21ff48842af5012
Winni Neessen <winfried.neessen@cleverbridge.com>
2023-12-22 16:08:02 +0100

    Correct typo in error.go comments
    The comments for the 'ErrCertConfigEmpty' variable in error.go contained a typo. This has been corrected to more accurately reflect the error that is returned when a TLS listener is configured without setting certificate or key paths.

--- error.go
+++ error.go
@@ -6,5 +6,6 @@ package logranger
 
 import "errors"
 
-// ErrCertConfigEmpty is returned if a TLS listener is configured but not certificate or key paths are set
+// ErrCertConfigEmpty is returned if a TLS listener is configured but ot certificate
+// or key paths are set
 var ErrCertConfigEmpty = errors.New("certificate and key paths are required for listener type: TLS")
